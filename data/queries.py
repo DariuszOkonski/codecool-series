cd61342f4e0e896481dcd4f2e6309e03bd88689f
Dariusz Okonski <darek200180@gmail.com>
2021-04-29 00:00:52 +0100

    last improvements

--- data/queries.py
+++ data/queries.py
@@ -6,6 +6,6 @@ def get_shows():
 
 def get_most_rated():
     # return data_manager.execute_select('select * from shows order by rating desc limit 15;')
-    return data_manager.execute_select("select s.id, s.title, s.year, s.rating, STRING_AGG( gr.name, ' - ' ORDER BY gr.name) as genres, s.trailer, s.homepage  from shows as s inner join show_genres as sg on s.id = sg.show_id inner join genres as gr on sg.genre_id = gr.id group by s.id order by s.rating desc limit 15")
+    return data_manager.execute_select("select s.title, s.year, s.runtime, s.rating, STRING_AGG( gr.name, ' - ' ORDER BY gr.name) as genres, s.trailer, s.homepage  from shows as s inner join show_genres as sg on s.id = sg.show_id inner join genres as gr on sg.genre_id = gr.id group by s.id order by s.rating desc limit 15")
 
 
